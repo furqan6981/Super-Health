c960044db51680c5f923d63c7f0b1cb91c7c7b56
Furqan Ahmad <109405673+furqan6981@users.noreply.github.com>
2022-12-18 01:10:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,102 @@
+
+    let navLinks = document.getElementById('navLinks');
+
+    function showMenu() {
+        navLinks.style.display = 'block';
+
+    }
+    function hideMenu() {
+        navLinks.style.display = '';
+    }
+
+
+    function myFunction() {
+        let x = document.getElementById("faq");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
+    function myFunction2() {
+        var x = document.getElementById("faq-1");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
+    function myFunction3() {
+        var x = document.getElementById("faq-2");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
+    function myFunction4() {
+        var x = document.getElementById("faq-3");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
+    function myFunction5() {
+        var x = document.getElementById("faq-4");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
+    function myFunction6() {
+        var x = document.getElementById("faq-5");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
+    function myFunction7() {
+        var x = document.getElementById("faq-6");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
+    function myFunction8() {
+        var x = document.getElementById("faq-7");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
+    function myFunction9() {
+        var x = document.getElementById("faq-8");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
+    function myFunction10() {
+        var x = document.getElementById("faq-9");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
